795a5fd99c66a81e8a4954b45a609db02c9f79bf
AfrinAkhtar <87437104+AfrinAkhtar@users.noreply.github.com>
2021-07-14 14:52:03 -0400

    Add files via upload

--- KwKPractice/KwKPractice/ViewController.swift
+++ KwKPractice/KwKPractice/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UILabel!
+    
+    
+    @IBOutlet weak var userInput: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButton(_ sender: UIButton) {
+        if let newTitle = userInput.text{
+            textField.text = newTitle
+        }
+    }
+    
+   
 }
 
