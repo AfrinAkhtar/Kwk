74abcd40c57cb9aa3810f896e8d879a96fa6573f
AfrinAkhtar <87437104+AfrinAkhtar@users.noreply.github.com>
2021-07-15 11:26:06 -0400

    Add files via upload

---
+++ AfrinMiniProject/AfrinMiniProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AfrinMiniProject
+//
+//  Created by Afrin Akhtar on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func infoButton(_ sender: UIButton) {
+    }
+    
+}
+
